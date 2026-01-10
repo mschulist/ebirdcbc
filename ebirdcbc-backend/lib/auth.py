3d6fab69450fa32dec13283ef33599772dbfa8d2
Mark Schulist <mschulist2@gmail.com>
2026-01-10 13:28:54 -0800

    Change timeout

--- ebirdcbc-backend/lib/auth.py
+++ ebirdcbc-backend/lib/auth.py
@@ -10,7 +10,7 @@ from .db import CBCDB
 
 SECRET_KEY = os.getenv("SECRET_KEY") or "test"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+ACCESS_TOKEN_EXPIRE_MINUTES = 30000
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
